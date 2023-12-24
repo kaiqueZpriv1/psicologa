05863e271722a0f478728b4a583e4323029a4771
kaiqueZpriv1 <kakamelo141205@gmail.com>
2023-12-24 20:08:12 -0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import Global from './style/Global';
 import { HeaderC } from './components/header/HeaderC';
 import { HomeC } from './components/home/HomeC';
 import { ServiceC } from './components/service/ServiceC';
+import { AboutC } from './components/about/AboutC';
+import { PlansC } from './components/plans/PlansC';
+import { ContactC } from './components/contact/ContactC';
 
 function App() {
 	return (
 		<div className='app'>
 			<Global />
 			<HeaderC />
-			<HomeC/>
-			<ServiceC/>
+			<HomeC />
+			<ServiceC />
+			<AboutC/>
+			<PlansC/>
+			<ContactC/>
 		</div>
 	);
 }
